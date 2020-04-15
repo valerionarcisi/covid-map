12097cf711013703637baec3e5c9bb3a0a495077
Valerio Narcisi <valerio.narcisi@gmail.com>
2020-04-15 16:09:59 +0200

    added reducers factory

---
+++ src/app/store/reducers.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+import provinces, { STATE_KEY as PROVINCE_STATE_KEY } from './ducks/provinces';
+
+const entities = combineReducers({
+  [PROVINCE_STATE_KEY]: provinces,
+});
+
+
+export default function reducersFactory() {
+  return combineReducers({
+    entities,
+  });
+}
