cda73f1b5b56f14331a9506ec750551202c25ab1
Valerio Narcisi <valerio.narcisi@gmail.com>
2020-04-16 14:52:57 +0200

    fix mock

--- mock/server.js
+++ mock/server.js
@@ -7,12 +7,9 @@ const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
 
-server.get('/data', (req, res) => {
+server.get('/dpc-covid19-ita-province-latest.json', (req, res) => {
   res.jsonp(db);
 });
 
