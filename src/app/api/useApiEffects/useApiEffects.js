9cebd854493fe3ca96a901e493f715b6b850260c
Valerio Narcisi <valerio.narcisi@gmail.com>
2020-04-16 14:58:32 +0200

    added cancled promise

--- src/app/api/useApiEffects/useApiEffects.js
+++ src/app/api/useApiEffects/useApiEffects.js
@@ -1,33 +1,37 @@
-import { useReducer, useEffect, useCallback } from 'react';
+import { useReducer, useEffect } from 'react';
 import reducer, { initialState } from './reducer';
 import { fetching, success, error } from './actions';
 
 const useApiEffects = (
   req,
   endpoint,
-  { verb = 'get', payload = null } = {},
-  immediate = true
+  { verb = 'get', payload = null } = {}
 ) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const makeRequest = useCallback(async () => {
-    if (typeof req[verb] !== 'function') return;
-    dispatch(fetching());
-
-    try {
-      const response = await req[verb](endpoint, { ...payload });
-      const res = await response.data;
-      dispatch(success(res));
-    } catch ({ response = null, ...exception }) {
-      dispatch(error(response, exception));
-    }
-  }, [req, verb, endpoint, payload]);
-
   useEffect(() => {
-    if (immediate) {
-      makeRequest();
-    }
-  }, [makeRequest, immediate]);
+    let didCanceled = false;
+    const makeRequest = async () => {
+      if (typeof req[verb] !== 'function' || typeof endpoint === undefined)
+        return;
+
+      if (!didCanceled) dispatch(fetching());
+
+      try {
+        const response = await req[verb](endpoint, { ...payload });
+        const res = await response.data;
+        if (!didCanceled) dispatch(success(res));
+      } catch ({ response = null, ...exception }) {
+        if (!didCanceled) dispatch(error(response, exception));
+      }
+    };
+
+    makeRequest();
+    return () => {
+      didCanceled = true;
+      return didCanceled;
+    };
+  }, [req, verb, endpoint, payload]);
 
   return state;
 };
