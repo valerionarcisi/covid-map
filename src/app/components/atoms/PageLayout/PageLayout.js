3d7cee2316b2f50ba2878477e68341ad7e9bf736
Valerio Narcisi <valerio.narcisi@gmail.com>
2020-04-17 11:02:44 +0200

    create pagelayout

---
+++ src/app/components/atoms/PageLayout/PageLayout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import pt from 'prop-types';
+import clsx from 'clsx';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  root: {
+    display: 'flex',
+    minHeight: '100vh',
+  },
+});
+
+function PageLayout({ children, className, ...others }) {
+
+  const classes = useStyles();
+
+  return (
+    <div className={clsx(classes.root, className)}>
+      {children}
+    </div>
+  );
+}
+
+PageLayout.propTypes = {
+  children: pt.node.isRequired,
+  className: pt.string,
+};
+
+export default PageLayout;
