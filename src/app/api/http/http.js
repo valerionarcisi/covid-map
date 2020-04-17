59880f7881d9a38a3a2e99de707cf94118f4c4db
Valerio Narcisi <valerio.narcisi@gmail.com>
2020-04-17 15:07:53 +0200

    delete tranformer

--- src/app/api/http/http.js
+++ src/app/api/http/http.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-axios.defaults.transformResponse.push((data) => (data ? data.data : null));
 
 export default axios;
