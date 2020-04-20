e53b896069ac5a4cc3e08684c10d119d30dc5324
Valerio Narcisi <valerio.narcisi@gmail.com>
2020-04-20 12:21:48 +0200

    added test for radius utility

---
+++ src/app/components/provinces/Province/Provice.test.js
@@ -0,0 +1,27 @@
+import { radius } from './Province';
+
+describe('#radius', () => {
+  it('#undefined', () => {
+    expect(radius()).toEqual(10);
+  });
+
+  it('#500', () => {
+    expect(radius(2)).toEqual(5);
+    expect(radius(500)).toEqual(5);
+  });
+
+  it('#5000', () => {
+    expect(radius(501)).toEqual(10);
+    expect(radius(5000)).toEqual(10);
+  });
+
+  it('#10000', () => {
+    expect(radius(5001)).toEqual(15);
+    expect(radius(10000)).toEqual(15);
+  });
+
+  it('# +10000', () => {
+    expect(radius(10001)).toEqual(20);
+    expect(radius(1000000)).toEqual(20);
+  });
+});
