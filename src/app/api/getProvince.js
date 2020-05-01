527daddbbac63a7a1b2406d7bc9eb00a7cf80d0a
Valerio Narcisi <valerio.narcisi@gmail.com>
2020-05-01 11:39:49 +0200

    fix url

--- src/app/api/getProvince.js
+++ src/app/api/getProvince.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default async function getProvince() {
-  const url = `https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json`
+  const url = `https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province-latest.json`
   const { data } = await axios.get(url)
   return data
 }
